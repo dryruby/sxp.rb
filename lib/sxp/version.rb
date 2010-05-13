583cbcb193d8d05ad2a44011aff9230cf836f868
Arto Bendiken <arto@bendiken.net>
2010-05-13 10:34:16 +0200

    Bumped version to 0.0.2.

--- lib/sxp/version.rb
+++ lib/sxp/version.rb
@@ -1,14 +1,23 @@
 module SXP
-  module VERSION #:nodoc:
+  module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 1
+    TINY  = 2
     EXTRA = nil
 
     STRING = [MAJOR, MINOR, TINY].join('.')
-    STRING << "-#{EXTRA}" if EXTRA
+    STRING << ".#{EXTRA}" if EXTRA
 
-    def self.to_s()   STRING end
+    ##
+    # @return [String]
+    def self.to_s() STRING end
+
+    ##
+    # @return [String]
     def self.to_str() STRING end
+
+    ##
+    # @return [Array(Integer, Integer, Integer)]
+    def self.to_a() [MAJOR, MINOR, TINY] end
   end
 end
