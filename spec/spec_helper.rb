309e64ea368bab8fa49fa681fea0496a307fb1d1
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-03-11 14:02:55 -0700

    Run specs with bundler.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
+require "bundler/setup"
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$:.unshift File.dirname(__FILE__)
+
+require "bundler/setup"
+require 'rspec'
 require 'sxp'
+
 include SXP
