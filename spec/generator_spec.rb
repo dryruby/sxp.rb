8f58c00b45b3995a7ec052c7bdb77c5e59e3718c
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-03-11 22:56:58 -0700

    Improve generated SXP output by folding non-list elements to previous line, where possible.

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -8,13 +8,12 @@ describe SXP::Generator do
         %{(bgp (triple s p o))\n}
       ],
       "long component" => [
-        [:thing, [:string, "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"]],
+        [:thing, [:string, "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"]],
         %{(thing
- (string
-  "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
- )
-)
-}
+           (string
+            "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+           ))
+          }.gsub(/^          /, '')
       ],
       "SPARQL" => [
         [:prefix,
@@ -29,22 +28,28 @@ describe SXP::Generator do
               [:bgp,[:triple, :"?s", :"?p", :"?o"]],
               [:graph, :"?g", [:bgp, [:triple, :"?s", :"?p", :"?o"]]]]]],
         %{(prefix
- ((: <http://example/>))
- (dataset
-  (<data-g1.ttl>
-   (named <data-g1.ttl>)
-   (named <data-g2.ttl>)
-   (named <data-g3.ttl>)
-   (named <data-g4.ttl>)
-  )
-  (union
-   (bgp (triple ?s ?p ?o))
-   (graph ?g (bgp (triple ?s ?p ?o)))
-  )
- )
-)
-}
+           ((: <http://example/>))
+           (dataset
+            (<data-g1.ttl>
+             (named <data-g1.ttl>)
+             (named <data-g2.ttl>)
+             (named <data-g3.ttl>)
+             (named <data-g4.ttl>))
+            (union (bgp (triple ?s ?p ?o)) (graph ?g (bgp (triple ?s ?p ?o))))) )
+          }.gsub(/^          /, '')
       ],
+      "EBNF" => [
+        [[:rule, :empty, "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"],
+         [:rule, :ebnf, "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"]],
+        %q{(
+           (rule empty
+            "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+           )
+           (rule ebnf
+            "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+           ))
+          }.gsub(/^          /, '')
+      ]
     }.each do |title, (input, expected)|
       it title do
         SXP::Generator.string(input).should == expected
