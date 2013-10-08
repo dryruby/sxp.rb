7718a5de28cf393b7c878704c172e9a203b6f8cf
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-10-07 17:06:07 -0700

    Change SXP.read* methods to use Reader::Basic rather than Reader::Scheme. Move scheme-specific specs from reader_spec to scheme_spec.

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,45 +1,28 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe SXP::Reader do
+describe SXP::Reader::Basic do
   context "when reading empty input" do
-    it "raises an error" do
-      lambda { read('') }.should raise_error(SXP::Reader::Error)
-      lambda { read(' ') }.should raise_error(SXP::Reader::Error)
-      lambda { read('#!/usr/bin/env sxp2json') }.should raise_error(SXP::Reader::Error)
-    end
-  end
-
-  context "when reading shebang scripts" do
-    it "ignores shebang lines" do
-      read_all("#!/usr/bin/env sxp2json\n(1 2 3)\n").should == [[1, 2, 3]]
+    [
+      '',
+      ' ',
+    ].each do |input|
+      it "raises an error on #{input.inspect}" do
+        expect {read(input)}.to raise_error(SXP::Reader::Error)
+      end
     end
   end
 
   context "when reading valid symbols" do
-    it "reads ':' as a symbol" do
-      read(':').should == :':'
-    end
-
-    it "reads '.' as a symbol" do
-      read('.').should == :'.'
-    end
-
-    it "reads '\\' as a symbol" do
-      read('\\').should == :'\\'
+    %w(: . \\ \n \t \r).each do |c|
+      it "reads #{c.inspect} as a symbol" do
+        expect(read(c)).to eq c.to_sym
+      end
     end
   end
 
   context "when reading lists" do
     it "reads '()' as an empty list" do
       read('()').should == []
-    end
-
-    it "reads '[]' as an empty list" do
-      read('[]').should == []
-    end
-
-    it "reads '[] 1' as an empty list" do
-      read('[] 1').should == []
     end
 
     it "reads '(1 2 3)' as a list" do
@@ -49,9 +32,19 @@ describe SXP::Reader do
     it "reads '(1 2 3) 4' as a list" do
       read('(1 2 3) 4').should == [1, 2, 3]
     end
+  end
 
-    it "reads '[1 2 3]' as a list" do
-      read('[1 2 3]').should == [1, 2, 3]
+  context "problematic examples" do
+    {
+      %q{"\t'[]()-"} => "\t'[]()-",
+      %q{(range "\t'[]()-")} =>
+        [:range, "\t'[]()-"],
+      %q{(seq "\"" (star (alt CHAR (range "\t'[]()-"))) "\"")} =>
+        [:seq, '"', [:star, [:alt, :CHAR, [:range, "\t'[]()-"]]], '"'],
+    }.each do |input, output|
+      it "reads #{input} as #{output.inspect}" do
+        expect(read(input)).to eq output
+      end
     end
   end
 
@@ -60,32 +53,16 @@ describe SXP::Reader do
       lambda { read_all('(1') }.should raise_error(SXP::Reader::Error)
     end
 
-    it "raises an error on '[1'" do
-      lambda { read_all('[1') }.should raise_error(SXP::Reader::Error)
-    end
-
     it "raises an error on '1)'" do
       lambda { read_all('1)') }.should raise_error(SXP::Reader::Error)
-    end
-
-    it "raises an error on '1]'" do
-      lambda { read_all('1]') }.should raise_error(SXP::Reader::Error)
-    end
-
-    it "raises an error on '(1]'" do
-      lambda { read_all('(1]') }.should raise_error(SXP::Reader::Error) # FIXME
-    end
-
-    it "raises an error on '[1)'" do
-      lambda { read_all('[1)') }.should raise_error(SXP::Reader::Error) # FIXME
     end
   end
 
   def read(input, options = {})
-    SXP.read(input.freeze, options)
+    SXP::Reader::Basic.read(input.freeze, options)
   end
 
   def read_all(input, options = {})
-    SXP.read_all(input.freeze, options)
+    SXP::Reader::Basic.read_all(input.freeze, options)
   end
 end
