3ed4391fdac1b8f634fed576f5aa962f8d96c128
Gregg Kellogg <gregg@greggkellogg.net>
2020-12-22 12:04:54 -0800

    Configure coveralls.

--- Gemfile
+++ Gemfile
@@ -8,3 +8,8 @@ group :debug do
   gem "byebug", platform: :mri
   gem 'awesome_print',    github: 'akshaymohite/awesome_print'
 end
+
+group :development, :test do
+  gem 'simplecov',  platforms: :mri
+  gem 'coveralls',  '~> 0.8', platforms: :mri
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,21 @@ $:.unshift File.dirname(__FILE__)
 
 require "bundler/setup"
 require 'rspec'
+
+begin
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ])
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+rescue LoadError => e
+  STDERR.puts "Coverage Skipped: #{e.message}"
+end
+
 require 'sxp'
 
 include SXP
